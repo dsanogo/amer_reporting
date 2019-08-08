4c760a75f1f385c4f7633df6d8729a91cfba5f83
Daouda Sanogo <m.fathy@unicomg.com>
2019-08-08 18:13:44 +0200

    Added public

--- resources/views/admin/inc/header.blade.php
+++ resources/views/admin/inc/header.blade.php
@@ -9,8 +9,8 @@
     <title></title>
 
     <!-- Bootstrap -->
-    <link href="{{asset('assets/css/bootstrap.min.css')}}" rel="stylesheet">
-    <link href="{{asset('assets/css/aamer.css')}}" rel="stylesheet">
+    <link href="{{asset('public/assets/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('public/assets/css/aamer.css')}}" rel="stylesheet">
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
         integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
