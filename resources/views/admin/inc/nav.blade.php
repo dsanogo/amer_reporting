2f10267fbcb34c04a5c127d55c2d5eb7a0f88177
Mahmoud Rabea <m.rabea@egyptunicom.com>
2019-08-28 21:52:27 +0200

    waleed

--- resources/views/admin/inc/nav.blade.php
+++ resources/views/admin/inc/nav.blade.php
@@ -9,17 +9,17 @@
     <div class="col-md-3 col-sm-4 col-xs-8 left-sied">
         <div class="col-md-5 col-sm-6 col-xs-6 dropdown p-l-30r" style=""> <a href="#" class="dropdown-toggle"
                 data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span
-                    class="caret"></span>احمد حسن </a>
-            <ul class="dropdown-menu">
+                    class="caret"></span>احمد حسن<img class="person hidden-xs" src="{{asset('public/assets/img/person-placeholder.png')}}" alt=""> </a>
+            <ul class="dropdown-menu ">
                 <li><a href="#">Action</a></li>
 
             </ul>
         </div>
 
         <div class="dropdown col-md-6 col-sm-6 col-xs-6">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+            <a class="sptop" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
                 aria-expanded="false"><span class="caret"></span>العربيه </a>
-            <ul class="dropdown-menu">
+            <ul class="dropdown-menu ">
                 <li><a href="#">Action</a></li>
 
             </ul>
@@ -39,7 +39,7 @@
             <li><a href="{{route('admin.index')}}"> اللوحه الرئيسيه</a></li>
             <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">تقارير جدولية <span class="caret"></span></a>
-                <ul class="dropdown-menu  fixaa text-right">
+                <ul class="dropdown-menu dropdown-menusa fixaa text-right">
                   <li><a href="{{route('show.reportCategories')}}">المعاملات خلال فترة حسب نوع المعاملة</a></li>
                   <li><a href="{{route('admin.getInvoicesByOffices')}}">المعاملات خلال فترة موزعة على المكاتب</a></li>
                   <li><a href="{{route('admin.getMobileAndOfficeInvoices')}}">استخدام نظام المحمول للحصول على الخدمة</a></li>
@@ -50,7 +50,7 @@
           
               <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">احصائيات وقياس الاداء<span class="caret"></span></a>
-                <ul class="dropdown-menu  fixaa text-right">
+                <ul class="dropdown-menu dropdown-menusa fixaa text-right">
                     <li><a href="{{route('admin.offices.details')}}">احصائيات مقارنة انتاجية المكاتب</a></li>
                     <li><a href="{{route('admin.monthlyInvoices')}}">التطور الزمني لانتاجية المكاتب</a></li>
                     <li><a href="{{route('admin.offices.ProcessTimeDetails')}}">قياس اداء المكاتب</a></li>
@@ -61,7 +61,7 @@
 
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">تقاير الذكاء الاصطناعي<span class="caret"></span></a>
-                    <ul class="dropdown-menu  fixaa text-right">
+                    <ul class="dropdown-menu dropdown-menusa fixaa text-right">
                         <li><a href="{{route('admin.offices.details')}}">ما هى المكاتب التى يسوء أداؤها وتستدعى التدخل؟</a></li>
                         <li><a href="{{route('admin.getMobileAndOfficeInvoicesQuarterly')}}">هل اصدار المعاملات بالمكاتب مفضل للمتعامل أم بنظام المحمول؟</a></li>
                         <li><a href="{{route('admin.offices.ProcessTimeDetails')}}">ما هى مواسم زيادة وقلة أعداد المعاملات؟</a></li>
